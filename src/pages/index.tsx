523a6e87b4130acc995a2ad694a936012803af31
masafumi330 <mihusamawasami@gmail.com>
2023-12-17 22:04:56 +0900

    :zap: html構造一通り描き終わる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,114 +5,86 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header>
+        <h1>
+          <Image src="/logo.svg" alt="logo" width="100" height="100" />
+          <span>Next.js</span>
+        </h1>
+        <a href="https://nextjs.org/docs">Documentation</a>
+      </header>
+      <main>
+        <div>
+          <div>
+            <p>さくっと、パス付き資料の作成&共有</p>
+            <ol>
+              <img src="" alt="" /><li>ストレスなくサクサク書ける</li>
+              <img src="" alt="" /><li>ドキュメントはパス付きで共有可能</li>
+              <img src="" alt="" /><li>ベータ版の期間は完全無料</li>
+            </ol>
+            <a href="">登録する</a><img src="" alt="" />
+          </div>
+          <div>
+
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          <h2>快適さへのこだわり</h2>
+          <div>
+            <ol>
+              <li>
+                <img src="" alt="" />
+                <p>シンプルな機能</p>
+                <p>多機能でない分、わかりやすく・使いやす機能。書くこと、共有することにのみ特化。</p>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <p>綺麗なドキュメント</p>
+                <p>見やすいドキュメント、見やすい管理画面。シンプルでわかりやすいデザイン。</p>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <p>書きやすいエディタ</p>
+                <p>ドキュメント作成画面の装飾はショートカットを用意。慣れればサクサク。</p>
+              </li>
+            </ol>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div>
+          <h2>.docの機能</h2>
+          <div>
+            <img src="" alt="" />
+            <p>ドキュメント作成</p>
+            <p>装飾はボタンをワンクリック、もしくはショートカットキーで、綺麗に作成可能。</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>webページとして共有可能</p>
+            <p>パスワード付き共有/一般公開（URLを知っていれば閲覧可）／非公開の3つを用意。</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>タグでドキュメントを管理</p>
+            <p>ドキュメントはタグ付けすることで、分類が可能。もちろん追加削除／複数指定も可能。</p>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div>
+          <ol>
+            <li>トップ</li>
+            <li>利用規約</li>
+            <li>プライバシーポリシー</li>
+            <li>運用会社</li>
+          </ol>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div>
+          <p>© 2021 .doc</p>
+        </div>
+      </footer>
+    </>
   )
 }
